bf61502b681a44b9f15c0917da16f8fc9ace3071
Alexandra-Myers <bia416cat@gmail.com>
2022-07-05 13:05:52 -0400

    some edits

--- src/main/java/net/alexandra/malechia/block/custom/EtherealGrassBlock.java
+++ src/main/java/net/alexandra/malechia/block/custom/EtherealGrassBlock.java
@@ -16,48 +16,54 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.chunk.light.ChunkLightProvider;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.NetherConfiguredFeatures;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 
-public class EtherealGrassBlock
-extends Block
-implements Fertilizable {
-    public EtherealGrassBlock(AbstractBlock.Settings settings) {
-        super(settings);
-		setDefaultState(this.getStateManager().getDefaultState());
-    }
+public class NyliumBlock extends Block implements Fertilizable {
+	public NyliumBlock(AbstractBlock.Settings settings) {
+		super(settings);
+	}
 
-    private static boolean stayAlive(BlockState state, WorldView world, BlockPos pos) {
-        BlockPos blockPos = pos.up();
-        BlockState blockState = world.getBlockState(blockPos);
-        int i = ChunkLightProvider.getRealisticOpacity(world, state, pos, blockState, blockPos, Direction.UP, blockState.getOpacity(world, blockPos));
-        return i < world.getMaxLightLevel();
-    }
+	private static boolean stayAlive(BlockState state, WorldView world, BlockPos pos) {
+		BlockPos blockPos = pos.up();
+		BlockState blockState = world.getBlockState(blockPos);
+		int i = ChunkLightProvider.getRealisticOpacity(world, state, pos, blockState, blockPos, Direction.UP, blockState.getOpacity(world, blockPos));
+		return i < world.getMaxLightLevel();
+	}
 
-    @Override
-    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (!EtherealGrassBlock.stayAlive(state, world, pos)) {
-            world.setBlockState(pos, ModBlocks.ETHEREAL_DIRT.getDefaultState());
-        }
-    }
+	@Override
+	public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		if (!stayAlive(state, world, pos)) {
+			world.setBlockState(pos, Blocks.NETHERRACK.getDefaultState());
+		}
 
-    @Override
-    public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
-        return world.getBlockState(pos.up()).isAir();
-    }
+	}
 
-    @Override
-    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
-        return true;
-    }
+	@Override
+	public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+		return world.getBlockState(pos.up()).isAir();
+	}
 
-    @Override
-    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
-        BlockState blockState = world.getBlockState(pos);
-        BlockPos blockPos = pos.up();
-        ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
-        if (blockState.isOf(ModBlocks.ETHEREAL_GRASS_BLOCK)) {
-            VegetationPlacedFeatures.GRASS_BONEMEAL.value().generate(world, chunkGenerator, random, blockPos);
-        }
-    }
+	@Override
+	public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+		return true;
+	}
+
+	@Override
+	public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+		BlockState blockState = world.getBlockState(pos);
+		BlockPos blockPos = pos.up();
+		ChunkGenerator chunkGenerator = world.getChunkManager().getChunkGenerator();
+		if (blockState.isOf(Blocks.CRIMSON_NYLIUM)) {
+			((ConfiguredFeature)NetherConfiguredFeatures.CRIMSON_FOREST_VEGETATION_BONEMEAL.value()).generate(world, chunkGenerator, random, blockPos);
+		} else if (blockState.isOf(Blocks.WARPED_NYLIUM)) {
+			((ConfiguredFeature)NetherConfiguredFeatures.WARPED_FOREST_VEGETATION_BONEMEAL.value()).generate(world, chunkGenerator, random, blockPos);
+			((ConfiguredFeature)NetherConfiguredFeatures.NETHER_SPROUTS_BONEMEAL.value()).generate(world, chunkGenerator, random, blockPos);
+			if (random.nextInt(8) == 0) {
+				((ConfiguredFeature)NetherConfiguredFeatures.TWISTING_VINES_BONEMEAL.value()).generate(world, chunkGenerator, random, blockPos);
+			}
+		}
+
+	}
 }
